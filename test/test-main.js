21d53d3af1f55ddd281bde5b724329f923257fdb
knidarkness <knidarkness@gmail.com>
2018-01-08 05:22:06 +0200

    changed tests

--- test/test-main.js
+++ test/test-main.js
@@ -3,12 +3,16 @@ const request = require('request');
 const bloglight = require('./../');
 
 bloglight.createBlog('/', {username: 'admin@site.com', password: 'pass'}).then((blog) => {
-    blog.listen(80);
+    blog.listen(80, () => {});
 });
-it('Main page content', function(done) {
-    request('http://localhost:80' , function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-        process.exit();
+
+describe("Homepage test", function() {
+    it('Main page content', function(done) {
+        this.timeout(5000);
+        request('http://localhost:80' , function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+            process.exit();
+        });
     });
 });
