097f26a781a31878c98c1ec91a0a501954944882
knidarkness <knidarkness@gmail.com>
2018-01-08 05:31:29 +0200

    changed tests

--- test/test-main.js
+++ test/test-main.js
@@ -6,13 +6,13 @@ const bloglight = require('./../');
 describe("Homepage test", function() {
     before(function (done) {
         bloglight.createBlog('/', {username: 'admin@site.com', password: 'pass'}).then((blog) => {
-            blog.listen(80, () => {
+            blog.listen(8080, () => {
                 done();
             });
         });
     });
     it('Main page content', function(done) {
-        request('http://localhost:80' , function(error, response, body) {
+        request('http://localhost:8080' , function(error, response, body) {
             expect(response.statusCode).to.equal(200);
             done();
             process.exit();
